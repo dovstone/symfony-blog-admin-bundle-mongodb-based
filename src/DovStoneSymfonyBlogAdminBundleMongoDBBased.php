692bc33868c7b6480e48263ed8f24d51fde00d04
stOne <silveredovoui@gmail.com>
2023-02-21 05:44:35 +0000

    Create DovStoneSymfonyBlogAdminBundleMongoDBBased.php

---
+++ src/DovStoneSymfonyBlogAdminBundleMongoDBBased.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: stOne
+ * Date: 21/02/2022
+ * Time: 05:44
+ */
+
+namespace DovStone\Bundle\SymfonyBlogAdminBundleMongoDBBased;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DovStoneSymfonyBlogAdminBundleMongoDBBased extends Bundle
+{
+}
